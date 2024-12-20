2af74ca03f27a4379bf2890a2e4d64adfb505b8e
devang8320 <devangvasani03@gmail.com>
2024-12-19 20:22:18 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import React  from 'react'
+
+
+import './App.css'
+
+import Pass_generator from './login_generator'
+
+function App() {
+  
+
+  return (
+    
+    <Pass_generator/>
+   
+
+    
+  )
+}
+
+export default App
